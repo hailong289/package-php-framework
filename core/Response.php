e47acd837758d8e33abd87bfc0a312f5c6d87ab7
Hai Long <longdh2.dev@gmail.com>
2023-11-17 16:36:04 +0700

    update response

--- core/Response.php
+++ core/Response.php
@@ -3,18 +3,18 @@
 namespace App\Core;
 
 class Response {
-    public function redirectTo($path, $status = 302, $headers = []){
+    public static function redirectTo($path, $status = 302, $headers = []){
         header('Location: ' . $path, true, $status);
         exit();
     }
-    public function json($data = [], $status = 200, $headers = [], $options = 0){
+    public static function json($data = [], $status = 200, $headers = [], $options = 0){
         http_response_code($status);
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data);
         exit();
     }
 
-    public function view($view, $data = [], $status = 200){
+    public static function view($view, $data = [], $status = 200){
         http_response_code($status);
         return get_view($view, $data);
     }
