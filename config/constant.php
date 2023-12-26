7b591a48199ce732fa6300f0e13c929144d1c9a9
Hai Long <longdh2.dev@gmail.com>
2023-12-26 16:36:05 +0700

    add write log file

--- config/constant.php
+++ config/constant.php
@@ -1,5 +1,5 @@
 <?php
 define('URL_PATH', (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/');
 define('CONNECT_REDIS', false); // coming soon
-define('DEBUG_LOG', false);
+define('DEBUG_LOG', true);
 define('LANGUAGE', 'vi');
--- index.php
+++ index.php
@@ -7,6 +7,13 @@ try {
     $app = new App;
 }catch (Throwable $e) {
     $code = (int)$e->getCode();
+    $date = "\n\n[".date('Y-m-d H:i:s')."]: ";
+    if (!file_exists(__DIR__ROOT .'/storage')) {
+        if (!mkdir($concurrentDirectory = __DIR__ROOT . '/storage', 0777, true) && !is_dir($concurrentDirectory)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $concurrentDirectory));
+        }
+    }
+    file_put_contents(__DIR__ROOT .'/storage/debug.log',$date . $e, FILE_APPEND);
     http_response_code($code ? $code:500);
     throw $e;
 }
