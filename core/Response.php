4b780fab43b2c3e1d13f7d04d8c5cb84717c6a21
Hai Long <longdh2.dev@gmail.com>
2024-03-07 16:36:30 +0700

    FIX RuntimeException

--- core/Response.php
+++ core/Response.php
@@ -2,8 +2,6 @@
 
 namespace System\Core;
 
-use http\Exception\RuntimeException;
-
 class Response {
     public static function redirectTo($path, $status = 302, $headers = []){
         header('Location: ' . $path, true, $status);
@@ -16,7 +14,7 @@ class Response {
 
     public static function view($view, $data = [], $status = 200){
         if(!file_exists(__DIR__ROOT . '/App/Views/'.$views.'.view.php')){
-            throw new RuntimeException("File App/Views/$view.view.php does not exist", 500);
+            throw new \RuntimeException("File App/Views/$view.view.php does not exist", 500);
         }
         http_response_code($status);
         if(count($data)) $GLOBALS['share_date_view'] = $data;
