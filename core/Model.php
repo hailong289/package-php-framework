fc6266488e721b028fce2e39c7a3f53288d3142e
Hai Long <longdh2.dev@gmail.com>
2024-05-03 15:04:49 +0700

    clean code

--- core/Model.php
+++ core/Model.php
@@ -1,25 +1,4 @@
 <?php
 namespace System\Core;
-use System\Traits\QueryBuilder;
 
-abstract class Model extends Database
-{
-    use QueryBuilder;
-    private static $DB;
-    private static $query = '';
-
-    public function __construct()
-    {
-        self::$DB = new Database();
-    }
-
-    public static function custom($sql = ''){
-        self::$query = self::modelInstance()->query($sql);
-        return self::modelInstance();
-    }
-
-    public static function modelInstance() {
-        return new static();
-    }
-
-}
+abstract class Model extends Database { }
