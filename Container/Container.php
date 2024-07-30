d4999a7cdab999db823bb6065c3dc63e6f29853a
Hai Long <longdh2.dev@gmail.com>
2024-07-30 17:30:40 +0700

    Update container

--- Container/Container.php
+++ Container/Container.php
@@ -3,71 +3,120 @@ namespace Hola\Container;
 
 class Container
 {
+    /**
+     * The container's  instance.
+     *
+     * @var static
+     */
+    protected static $instance;
 
-    public function make($abstract, $class = null)
+
+    public static function instance()
     {
-        if (is_null($class)) {
-            $class = $abstract;
+        if (is_null(self::$instance)) {
+            self::$instance = new Container();
         }
-
-        return $this->build($class);
+        return self::$instance;
     }
 
-    public function build(string $class)
+    /**
+     * @param $callable -- string class and method name
+     * @param  array  $parameters
+     */
+    public function call($callable, $parameters = [])
     {
-        try {
-            $reflector = new ReflectionClass($class);
-        } catch (\ReflectionException $e) {
-            throw new \Exception("Target class [$class] does not exist.", 0, $e);
-        }
+        // set class name with namespace and method name
+        $this->resolveCallback($callable);
+        $methodReflection = new \ReflectionMethod($this->callbackClass, $this->callbackMethod);
+        $methodParams = $methodReflection->getParameters();
 
-        // If the type is not instantiable, such as an Interface or Abstract Class
-        if (! $reflector->isInstantiable()) {
-            throw new \Exception("Target [$class] is not instantiable.");
-        }
-
-        $constructor = $reflector->getConstructor();
-
-        // If there are no constructor, that means there are no dependencies
-        if ($constructor === null) {
-            return new $class;
-        }
-
-        $parameters = $constructor->getParameters();
         $dependencies = [];
 
-        foreach ($parameters as $parameter) {
-            $type = $parameter->getType();
+        // loop with dependencies/parameters
+        foreach ($methodParams as $param) {
 
-            if (! $type instanceof ReflectionNamedType || $type->isBuiltin()) {
-                // Resolve a non-class hinted primitive dependency.
-                if ($parameter->isDefaultValueAvailable()) {
-                    $dependencies[] = $parameter->getDefaultValue();
-                } else if ($parameter->isVariadic()) {
-                    $dependencies[] = [];
-                } else {
-                    throw new \Exception("Unresolvable dependency [$parameter] in class {$parameter->getDeclaringClass()->getName()}");
+            $type = $param->getType(); // check type
+
+            if ($type && $type instanceof \ReflectionNamedType) { /// if parameter is a class
+                $name = $param->getClass()->newInstance(); // create insrance
+                array_push($dependencies, $name); // push  to $dependencies array
+
+            } else {  /// Normal parameter
+                $name = $param->getName();
+
+                if (array_key_exists($name, $parameters)) { // check exist in $parameters
+                    array_push($dependencies, $parameters[$name]); // push  to $dependencies array
                 }
             }
 
-            $name = $type->getName();
-
-            // Resolve a class based dependency from the container.
-            try {
-                $dependency = $this->get($name);
-                $dependencies[] = $dependency;
-            } catch (\Exception $e) {
-                if ($parameter->isOptional()) {
-                    $dependencies[] = $parameter->getDefaultValue();
-                } else {
-                    $dependency = $this->build($parameter->getType()->getName());
-                    $this->set($name, $dependency);
-                    $dependencies[] = $dependency;
-                }
-            }
         }
 
-        return $reflector->newInstanceArgs($dependencies);
+        // make class instance
+        $initClass = $this->make($this->callbackClass, $parameters);
+
+        // call method with $dependencies/parameters
+        return $methodReflection->invoke($initClass, ...$dependencies);
+    }
+
+
+    /**
+     * separate class and method name
+     * @param $callback
+     */
+    private function resolveCallback($callback)
+    {
+        //separate class and method
+        if (is_string($callback)) {
+            $segments = explode('@', $callback);
+        } else {
+            $segments = $callback;
+        }
+
+        // set class name with namespace
+        $this->callbackClass = $segments[0];
+
+        // set method name . if method name not provided then default method __invoke
+        $this->callbackMethod = isset($segments[1]) ? $segments[1] : '__invoke';
+
+    }
+
+
+    public function make($class, $parameters = [])
+    {
+
+        $classReflection = new \ReflectionClass($class);
+        $constructorParams = $classReflection->getConstructor()->getParameters();
+        $dependencies = [];
+
+        /*
+         * loop with constructor parameters or dependency
+         */
+        foreach ($constructorParams as $constructorParam) {
+
+            $type = $constructorParam->getType();
+
+            if ($type && $type instanceof \ReflectionNamedType) {
+
+                // make instance of this class :
+                $paramInstance = $constructorParam->getClass()->newInstance();
+
+                // push to $dependencies array
+                array_push($dependencies, $paramInstance);
+
+            } else {
+
+                $name = $constructorParam->getName(); // get the name of param
+                // check this param value exist in $parameters
+                if (array_key_exists($name, $parameters)) { // if exist
+                    // push  value to $dependencies sequencially
+                    array_push($dependencies, $parameters[$name]);
+
+                }
+            }
+
+        }
+        // finally pass dependancy and param to class instance
+        return $classReflection->newInstance(...$dependencies);
     }
 
 }
