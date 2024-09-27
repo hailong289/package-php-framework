51d48158f471904f4148b3b679312ae03e7c3e4b
Hai Long <longdh2.dev@gmail.com>
2024-09-27 17:33:20 +0700

    perf: improve middleware

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -26,47 +26,10 @@ class BaseController extends \stdClass {
             }
         }
     }
-    // Render ra view
-    public function render_view($views, $data = [])
-    {
-        // Đổi key mảng thành biến
-        if(count($data)) $GLOBALS['share_date_view'] = $data;
-        extract($data);
-        $views = preg_replace('/([.]+)/', '/' , $views);
-        if(file_exists(__DIR__ROOT . '/App/Views/'.$views.'.view.php')){
-            require_once __DIR__ROOT . '/App/Views/'.$views.'.view.php';
-        }
-        return $this;
-    }
 
     public function middleware($name)
     {
-        $kernel = new \Middleware\Kernel();
-        try {
-            if(!empty($kernel->routerMiddleware[$name])){
-                $class = $kernel->routerMiddleware[$name];
-                $call_middleware = new $class();
-                $handler = $call_middleware->handle(new Request());
-                $return_middleware = is_array($handler) ? (object)$handler:(is_bool($handler) ? $handler:$handler);
-                if($return_middleware->error_code){
-                    echo json_encode($return_middleware);
-                    exit();
-                }
-            }else{
-                echo json_encode([
-                    'error_code' => 1,
-                    'message' => "Middleware $name does not exist"
-                ]);
-                exit();
-            }
-        }catch (\Throwable $e) {
-            echo json_encode([
-                'error_code' => 1,
-                'message' => $e->getMessage(),
-                'file' => $e->getFile(),
-                'trace' => $e->getTraceAsString(),
-            ]);
-            exit();
-        }
+        $middleware = new Middleware();
+        $middleware->set($name);
     }
 }
