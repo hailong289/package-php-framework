7d8286e990d041642bfbab46a7d501d8da094049
Hai Long <longdh2.dev@gmail.com>
2024-10-07 17:17:15 +0700

    refactor: do not use model basecontroller

--- Core/BaseController.php
+++ Core/BaseController.php
@@ -1,32 +1,6 @@
 <?php
 namespace Hola\Core;
-class BaseController extends \stdClass {
-    public function model($names) {
-        $result = [];
-        if (is_array($names)) {
-            foreach ($names as $name){
-                $variable = str_replace('App\\Models\\','', $name);
-                $model = $name;
-                if(class_exists($model)){
-                    $model = new $model();
-                    $this->{$variable} = new $model();
-                    return $this->{$variable};
-                }else{
-                    throw new \RuntimeException("Model $name does not exits", 500);
-                }
-            }
-        } else {
-            $model = $names;
-            if(class_exists($model)){
-                $model = new $model();
-                $this->{$variable} = new $model();
-                return $this->{$variable};
-            }else{
-                throw new \RuntimeException("Model $model does not exits", 500);
-            }
-        }
-    }
-
+class BaseController {
     public function middleware($name)
     {
         $middleware = new Middleware();
