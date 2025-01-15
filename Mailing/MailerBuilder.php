21e3f56f22fb1c71e1d79af213cbe57e74dea65a
Hải Long <longdh2.dev@gmail.com>
2025-01-15 15:46:05 +0700

    refactor: mail builder

--- Mailing/MailerBuilder.php
+++ Mailing/MailerBuilder.php
@@ -10,14 +10,31 @@ class MailerBuilder extends Mailer {
 
     public function send()
     {
+        $mailFrom = conval('MAIL_FROM_ADDRESS', null);
+        $mailFromName = conval('MAIL_FROM_NAME', null);
         try {
+            if ($mailFrom) {
+                $this->from($mailFrom, $mailFromName);
+            }
+
             if (method_exists($this,'title')) {
                 $this->setSubject($this->title());
             }
+
             if (method_exists($this,'view')) {
                 $this->withHtml();
                 $this->setBody($this->view());
             }
+
+            if (method_exists($this,'mailFrom')) {
+                $name = method_exists($this,'mailFromName') ? $this->mailFromName() : null;
+                $this->from($this->mailFrom(), $name);
+            }
+
+            if (method_exists($this,'mailTo')) {
+                $this->to($this->mailTo());
+            }
+
             $this->work();
             return true;
         } catch (\Throwable $e) {
