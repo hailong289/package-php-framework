7c13eef468634e80031f9f922a5383d7cc31381d
Hải Long <longdh2.dev@gmail.com>
2025-04-18 14:40:35 +0700

    feat: change use cdn with css

--- view/error.view.php
+++ view/error.view.php
@@ -1,66 +1,28 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $code ?? 500 ?> Error Page </title>
-    <style>
-        * {
-            font-family: sans-serif;
-            color: rgba(0,0,0,0.75);
-        }
-        body {
-            margin: 0;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            height: 100vh;
-            padding: 0px 30px;
-            background: #ddd;
-        }
-
-        .wrapper {
-            max-width: 960px;
-            width: 100%;
-            margin: 30px auto;
-            transform: scale(0.8);
-        }
-        .landing-page {
-            max-width: 960px;
-            height: 475px;
-            margin: 0;
-            box-shadow: 0px 0px 8px 1px #ccc;
-            background: #fafafa;
-            border-radius: 8px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-        h1 {
-            font-size: 48px;
-            margin: 0;
-        }
-        p {
-            font-size: 16px;
-            width: 35%;
-            margin-bottom: 5px;
-            text-align: center;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oops! Something is wrong </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@longdh2/hola-framework@1.0.0/main.css">
 </head>
-<body>
-<div class="wrapper">
-    <div class="landing-page">
-        <div style="text-align:center;" class="icon__download"></div>
-        <h1> <?= $code ?? 500 ?></h1>
-        <p>Message: <?= $message ?? '' ?></p>
-        <p>Line: <?= $line ?? '' ?></p>
-        <p>File:  <?= $file ?? '' ?></p>
-        <p>Trace:  <?= $trace ?? '' ?></p>
+<body class="hl-page-error">
+<div class="error-container">
+    <div class="error-code"><?= $code ?? 500 ?></div>
+    <div class="error-summary"><?= $message ?? '' ?></div>
+    <div class="error-location">
+        <?php if (isset($file)): ?>
+            <strong>File: <strong><?= $file ?? '' ?> </strong> </strong>
+        <?php endif; ?>
+        <?php if (isset($line)): ?>
+            <strong>Line: <strong> <?= $line ?? '' ?> </strong> </strong>
+        <?php endif; ?>
     </div>
+    <?php if (isset($trace)): ?>
+        <div class="stack-trace">
+            Trace: <?= $trace ?? '' ?>
+        </div>
+    <?php endif; ?>
 </div>
 </body>
 </html>
