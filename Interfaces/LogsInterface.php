584dfb110af779a1254bea85e62b54d96bbd3d1a
Hải Long <longdh2.dev@gmail.com>
2025-05-26 21:45:45 +0700

    feat: refactor write log

--- Interfaces/LogsInterface.php
+++ Interfaces/LogsInterface.php
@@ -4,7 +4,7 @@ namespace Hola\Interfaces\InterfaceLogs;
 interface Log {
     public function dump(...$args);
     public function dump_html(...$args);
-    public function write($data, $name_file = 'debug');
+    public function write(array $data, $name_file = 'debug');
     public function debug($data);
     public function write_error($data, $name_file = 'debug');
 }
